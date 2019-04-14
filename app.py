eb5224a6dab600d686e4032fcd490424d24bcbec
Maku <makusu2@gmail.com>
2019-04-13 21:35:03 -0400

    Removed gross UI stuff

--- app.py
+++ app.py
@@ -8,26 +8,6 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 import random
 
-class SigninForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField("Submit")
-
-    def validate(self):
-        if not FlaskForm.validate(self):
-            return False
-        user = User.query.filter_by(username = self.username.data).first()
-        if user:
-            if user.is_correct_password(self.password.data):
-                return True
-            self.username.errors.append("Wrong password")
-            return False
-        self.username.errors.append("Unknown username")
-
-class SignupForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField("Submit")
 
 """
 Therapists should be added by the server manually, so there's no "Create new user" function
