3423c87e1c96f79568f6115ad3603c984c2ffdf8
arades79 <scravers@protonmail.com>
2020-02-23 08:07:17 -0500

    add serialization amd deserialzation of public key

--- hyperdome_client/encryption.py
+++ hyperdome_client/encryption.py
@@ -22,7 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 # considering using pyca/cryptography instead
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.serialization import Encoding, PublicFormat
+from cryptography.hazmat.primitives.serialization import Encoding, PublicFormat, load_pem_public_key
 from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 from cryptography.fernet import Fernet
@@ -38,21 +38,22 @@ class LockBox():
         self._shared_secret = None
 
     def get_public_key(self):
-        self._private_key.public_key(Encoding.PEM, PublicFormat.SubjectPublicKeyInfo)
+        self._private_key.public_key.public_bytes(Encoding.PEM, PublicFormat.SubjectPublicKeyInfo)
 
-    def make_shared_secret(self, public_key):
+    def make_shared_secret(self, public_key_bytes: bytes):
+        public_key = load_pem_public_key(public_key_bytes, default_backend())
         shared = self._private_key.exchange(ec.ECDH(), public_key)
         key_gen = HKDF(algorithm=hashes.SHA3_512(), length=32, salt=None, info=b'handshake', backend=default_backend())
         self._shared_secret = Fernet(key_gen.derive(shared))
 
-    def encrypt_message(self, message):
+    def encrypt_message(self, message) -> bytes:
         if isinstance(message, str):
             message = message.encode()
         elif not isinstance(message, bytes):
             raise TypeError
         return self._shared_secret.encrypt(message)
 
-    def decrypt_message(self, message):
+    def decrypt_message(self, message) -> str:
         if isinstance(message, str):
             message = message.encode()
         elif not isinstance(message, bytes):
