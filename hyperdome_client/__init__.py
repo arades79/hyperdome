0151cd9c79150a4f6f4fc6125669876dbf246841
Skyelar Craver <scravers@protonmail.com>
2020-03-23 23:03:34 -0400

    add todos

--- hyperdome_client/__init__.py
+++ hyperdome_client/__init__.py
@@ -67,6 +67,7 @@ def main():
     # to pass alternate settings (which might contain a preferred locale).
     # If an alternate --config is passed, we'll reload strings later.
     common.load_settings()
+    # TODO: remove or rebuild strings
     strings.load_strings(common)
 
     # Display OnionShare banner
@@ -93,6 +94,7 @@ def main():
         "--debug", action="store_true", dest="debug", help=strings._("help_debug")
     )
     parser.add_argument(
+        # TODO: default should be empty string for consistant typing
         "--config", metavar="config", default=False, help=strings._("help_config")
     )
     args = parser.parse_args()
