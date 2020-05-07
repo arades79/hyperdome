2eb7366ae2f2d5c303914deb270f4d5f43c75b34
Skyelar Craver <scravers@protonmail.com>
2020-05-06 23:12:18 -0400

    add bootstrap test

---
+++ test/hyperdome_common_test/test_bootstrap.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Hyperdome
+
+Copyright (C) 2019-2020 Skyelar Craver <scravers@protonmail.com>
+                   and Steven Pitts <makusu2@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import hyperdome.common.bootstrap as bs
+from hypothesis import given
+import hypothesis.strategies as st
+
+
+@given(st.one_of(st.none(), st.text(), st.binary(), st.integers(), st.floats()))
+def test_bootstrap(value):
+    @bs.bootstrap
+    def result():
+        return value
+
+    assert result is value
