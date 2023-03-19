36a1577765811d8e3e6e950c9355955697b72899
Skyelar Craver <scravers@protonmail.com>
2023-03-19 14:35:18 -0400

    Revert "Change client network connectivity (#114)" (#115)
    This reverts commit 69aa1f79e12400ae3d133e8959e467db4eab8636.

--- test/hyperdome_common_test/test_encryption.py
+++ test/hyperdome_common_test/test_encryption.py
@@ -19,161 +19,137 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-from datetime import timedelta
-import secrets
 import hyperdome.common.encryption as enc
 import pytest
-from hypothesis import given, assume, settings
+from typing import Tuple, Callable
+from hypothesis import given, assume
 import hypothesis.strategies as st
+import cryptography
 
-UserPair = tuple[enc.GuestKeyring, enc.CounselorKeyring]
+user_fixture = Callable[..., Tuple[enc.LockBox, enc.LockBox]]
 
 
 @pytest.fixture(scope="module")
-def pre_exchanged_users() -> UserPair:
+def pre_exchanged_user_factory():
+    def factory():
+        user_1_crypto = enc.LockBox()
+        user_2_crypto = enc.LockBox()
 
-    guest = enc.GuestKeyring()
-    counselor = enc.CounselorKeyring()
+        user_1_pub_key = user_1_crypto.public_chat_key
+        user_2_pub_key = user_2_crypto.public_chat_key
 
-    key_bundle = counselor.pre_key_bundle
-    pub_signing_key = enc.PubKeyBytes(
-        counselor.public_signing_key.public_bytes(
-            enc.Encoding.Raw, enc.PublicFormat.Raw
-        )
-    )
-    ot_key = enc.PubKeyBytes(secrets.choice(key_bundle.one_time_keys))
-    eph_key = enc.PubKeyBytes(
-        guest.public_key.public_bytes(enc.Encoding.Raw, enc.PublicFormat.Raw)
-    )
+        user_2_crypto.perform_key_exchange(user_1_pub_key, False)
+        user_1_crypto.perform_key_exchange(user_2_pub_key, True)
 
-    counselor.exchange(
-        enc.IntroductionMessage(ephemeral_key=eph_key, one_time_key=ot_key)
-    )
-    guest.exchange(
-        enc.KeyExchangeBundle(
-            one_time_key=ot_key,
-            pre_key_signature=key_bundle.pre_key_signature,
-            signed_pre_key=key_bundle.signed_pre_key,
-            pub_signing_key=pub_signing_key,
-        )
-    )
+        return user_1_crypto, user_2_crypto
 
-    return guest, counselor
+    return factory
 
 
 @given(message=st.text())
-def test_encrypt_decrypt_message(pre_exchanged_users: UserPair, message: str):
-    guest, counselor = pre_exchanged_users
-
-    guest_enc_message = guest.encrypt_message(message.encode())
-    counselor_enc_message = counselor.encrypt_message(message.encode())
-
-    assert (
-        message.encode()
-        != guest_enc_message.ciphertext
-        != counselor_enc_message.ciphertext
-    )
-
-    dec_guest_message = counselor.decrypt_message(guest_enc_message)
-    dec_counselor_message = guest.decrypt_message(counselor_enc_message)
-
-    assert message.encode() == dec_counselor_message == dec_guest_message
-
-
-@given(message=st.text())
-def test_key_rotation(pre_exchanged_users: UserPair, message: str):
-    guest, counselor = pre_exchanged_users
-
-    sent_message_1 = guest.encrypt_message(message.encode())
-    sent_message_2 = guest.encrypt_message(message.encode())
-
-    assert sent_message_1.ciphertext != sent_message_2.ciphertext
-
-    recieved_message_1 = counselor.decrypt_message(sent_message_1)
-    recieved_message_2 = counselor.decrypt_message(sent_message_2)
-
-    assert recieved_message_1 == recieved_message_2 == message.encode()
-
-
-@given(message=st.text())
-def test_no_double_decrypt(pre_exchanged_users: UserPair, message: str):
-    guest, counselor = pre_exchanged_users
-
-    enc_message = guest.encrypt_message(message.encode())
-
-    counselor.decrypt_message(enc_message)
-
-    with pytest.raises(ValueError) as _:
-        counselor.decrypt_message(enc_message)
-
-
-@given(message_1=st.text(), message_2=st.text(), message_3=st.text())
-def test_out_of_order_recieve(
-    pre_exchanged_users: UserPair, message_1: str, message_2: str, message_3: str
+def test_encrypt_decrypt_message(
+    pre_exchanged_user_factory: user_fixture, message: str
 ):
-    guest, counselor = pre_exchanged_users
+    user_1_crypto, user_2_crypto = pre_exchanged_user_factory()
 
-    enc_message_1 = guest.encrypt_message(message_1.encode())
-    enc_message_2 = guest.encrypt_message(message_2.encode())
-    enc_message_3 = guest.encrypt_message(message_3.encode())
+    enc_message_1 = user_1_crypto.encrypt_outgoing_message(message)
+    enc_message_2 = user_2_crypto.encrypt_outgoing_message(message)
 
-    assert counselor.decrypt_message(enc_message_3) == message_3.encode()
-    assert counselor.decrypt_message(enc_message_1) == message_1.encode()
-    assert counselor.decrypt_message(enc_message_2) == message_2.encode()
+    assert message != enc_message_1 != enc_message_2
+
+    dec_message_1 = user_2_crypto.decrypt_incoming_message(enc_message_1)
+    dec_message_2 = user_1_crypto.decrypt_incoming_message(enc_message_2)
+
+    assert message == dec_message_1 == dec_message_2
+
+
+@given(message=st.text())
+def test_key_rotation(pre_exchanged_user_factory: user_fixture, message: str):
+    user_1_crypto, user_2_crypto = pre_exchanged_user_factory()
+
+    sent_message_1 = user_1_crypto.encrypt_outgoing_message(message)
+    sent_message_2 = user_1_crypto.encrypt_outgoing_message(message)
+
+    assert sent_message_1 != sent_message_2
+
+    recieved_message_1 = user_2_crypto.decrypt_incoming_message(sent_message_1)
+    recieved_message_2 = user_2_crypto.decrypt_incoming_message(sent_message_2)
+
+    assert recieved_message_1 == recieved_message_2 == message
+
+
+@given(message=st.text())
+def test_no_double_decrypt(pre_exchanged_user_factory: user_fixture, message: str):
+    user_1_crypto, user_2_crypto = pre_exchanged_user_factory()
+
+    enc_message = user_1_crypto.encrypt_outgoing_message(message)
+
+    user_2_crypto.decrypt_incoming_message(enc_message)
+
+    with pytest.raises(cryptography.fernet.InvalidToken) as e:
+        user_2_crypto.decrypt_incoming_message(enc_message)
+
+
+@given(message=st.text())
+def test_rotation_cannot_decrypt(
+    pre_exchanged_user_factory: user_fixture, message: str
+):
+    user_1_crypto, user_2_crypto = pre_exchanged_user_factory()
+
+    enc_message = user_1_crypto.encrypt_outgoing_message(message)
+
+    prev_chat_key = user_2_crypto._chat_key
+
+    _ = user_2_crypto.public_chat_key
+
+    new_chat_key = user_2_crypto._chat_key
+
+    assert prev_chat_key != new_chat_key
+
+    # key is set to None, so TypeError is raised
+    with pytest.raises(TypeError) as e:
+        user_2_crypto.decrypt_incoming_message(enc_message)
+
+
+def test_signing_key_rotation():
+    user = enc.LockBox()
+    user.make_signing_key()
+    pub_key_1 = user.public_signing_key
+    user.make_signing_key()
+    pub_key_2 = user.public_signing_key
+
+    assert pub_key_1 != pub_key_2
 
 
 @given(st.text(), st.text())
-@settings(deadline=timedelta(milliseconds=500), max_examples=10)
 def test_other_passphrases_cannot_import(passphrase_1: str, passphrase_2: str):
     assume(passphrase_1 != passphrase_2)
     assume(passphrase_1 and passphrase_2)
-    counselor = enc.CounselorKeyring()
+    user = enc.LockBox()
+    user.make_signing_key()
 
-    exported_key = counselor.export_private_key(passphrase_1.encode())
+    user_key = user.export_key(passphrase_1)
 
-    with pytest.raises(ValueError) as _:
-        enc.CounselorKeyring(exported_key, passphrase_2.encode())
+    with pytest.raises(ValueError) as e:
+        user.import_key(user_key, passphrase_2)
+
+    user.import_key(user_key, passphrase_1)
 
 
 @given(st.text())
-@settings(deadline=timedelta(milliseconds=500), max_examples=10)
 def test_import_export_same_pub_key(passphrase: str):
     assume(passphrase)
 
-    counselor = enc.CounselorKeyring()
+    user = enc.LockBox()
+    user.make_signing_key()
+    initial_pub_key = user.public_signing_key
 
-    exported_key = counselor.export_private_key(passphrase.encode())
+    exported_key = user.export_key(passphrase)
 
-    counselor_2 = enc.CounselorKeyring(exported_key, passphrase.encode())
+    user.make_signing_key()
 
-    counselor_1_key_bytes = counselor.public_signing_key.public_bytes(
-        enc.Encoding.Raw, enc.PublicFormat.Raw
-    )
-    counselor_2_key_bytes = counselor_2.public_signing_key.public_bytes(
-        enc.Encoding.Raw, enc.PublicFormat.Raw
-    )
+    assert user.public_signing_key != initial_pub_key
 
-    assert counselor_1_key_bytes == counselor_2_key_bytes
+    user.import_key(exported_key, passphrase)
 
-
-@given(st.binary(min_size=32, max_size=32))
-def test_x25519_from_ed25519(key_bytes: bytes):
-    from hyperdome.common.key_conversion import (
-        x25519_from_ed25519_private_key,
-        x25519_from_ed25519_public_key,
-        Encoding,
-        PublicFormat,
-        Ed25519PrivateKey,
-    )
-
-    pvk_ed = Ed25519PrivateKey.from_private_bytes(key_bytes)
-    pbk_ed = pvk_ed.public_key()
-
-    pvk_x = x25519_from_ed25519_private_key(pvk_ed)
-
-    pbk_x1 = pvk_x.public_key()
-    pbk_x2 = x25519_from_ed25519_public_key(pbk_ed)
-
-    assert pbk_x1.public_bytes(Encoding.Raw, PublicFormat.Raw) == pbk_x2.public_bytes(
-        Encoding.Raw, PublicFormat.Raw
-    )
+    assert user.public_signing_key == initial_pub_key
